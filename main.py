f23b192264e792d048c3e5b2010069c397a4a018
JoaoPedro-Ramos <joaopedro.r4mos@gmail.com>
2023-04-09 00:50:33 -0300

    gerador de senhas criado

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-from random import choice
+from secrets import choice
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm',
             'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 caracters = ['!', '@', '#', '$', '%', '&', '*', '_', '+', '-', '=', '~']
 
 _list = alphabet + numbers + caracters
 passwords_length = 15
-password = ''
-
-for i in range(passwords_length):
-    password += choice(_list)
-
-print(password)
+password = [choice(_list) for i in range(passwords_length)]
+password = ''.join(password)
