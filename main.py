eaed85dfb4951cda2896cff1d6a6dbc6e40ad9b8
JoaoPedro-Ramos <joaopedro.r4mos@gmail.com>
2023-04-13 00:18:04 -0300

    Conexão com banco de dados sqlite para armazenamento das senhas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from secrets import choice
 import string
+import sqlite3
 
 def create_list():
     _list = string.ascii_letters + string.digits + string.punctuation
@@ -9,3 +10,31 @@ def create_password(password_length=8, _list=create_list()):
     password = [choice(_list) for i in range(password_length)]
     return ''.join(password)
 
+def create_database():
+    database = sqlite3.connect('database.db')
+    return database
+
+def queries():
+    database = create_database()
+    operator = database.cursor()
+    name, select = inputs()
+    insert(database, operator, name)
+    if select:
+        selects(operator)
+    database.close()
+
+def insert(database, operator, name):
+    operator.execute(f'INSERT INTO passwords (NAME, PASSWORD)VALUES("{name}", "{create_password()}")')
+    database.commit()
+
+def selects(operator):
+    operator.execute('SELECT * FROM passwords')
+    print(operator.fetchall())
+
+def inputs():
+    name = input('name: ')
+    select = bool(input('consult '))
+    return name, select
+
+if __name__ == '__main__':
+    queries()
